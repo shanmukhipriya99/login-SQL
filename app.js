9dc9404dfe5abfb529b7cc39666a419252ccea91
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-07 19:57:38 +0530

    Working code

---
+++ app.js
@@ -0,0 +1,20 @@
+//Importing packages
+const express = require("express");
+require("./db/db");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//Importing routes
+const users = require("./routes/users");
+const list = require("./routes/list");
+
+//Importing middleware
+app.use(express.json());
+app.use(users);
+app.use(list);
+
+//Listening...
+app.listen(port, () => {
+    console.log("Serving on port " + port);
+});
